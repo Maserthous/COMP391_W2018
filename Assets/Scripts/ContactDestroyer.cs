01a6b7c241511c3f3a009890b987396e2e97754f
Maserthous <35930761+Maserthous@users.noreply.github.com>
2018-03-21 14:14:17 -0400

    Added UI, restart and quit; changed skull; and built

--- Assets/Scripts/ContactDestroyer.cs
+++ Assets/Scripts/ContactDestroyer.cs
@@ -5,15 +5,50 @@ using UnityEngine;
 public class ContactDestroyer : MonoBehaviour {
 
     public GameObject explosionSkull;
-	void OnTriggerEnter2D(Collider2D other)
+    public GameObject explosionPlayer;
+    public int score;
+
+    private GameController gameControllerScript;
+
+    private void Start()
     {
-        if(other.tag=="Boundary")
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+
+        if (gameControllerObject != null)
+        {
+            gameControllerScript = gameControllerObject.GetComponent<GameController>();
+        }
+        else
+            Debug.Log("Cannot find Game Controller script on Object");
+
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boundary" || other.tag == "Hazard")
         {
             return;
             // Debug.Log("DestroyByContact");
         }
 
-        Instantiate(explosionSkull, this.transform.position, this.transform.rotation);
+        if (other.tag == "Player")
+        {
+            Vector3 deltaP = (this.transform.position + other.transform.position) / 2; // Delta of Player and this
+            
+            // Create our explosion animation
+            Instantiate(explosionPlayer, deltaP, other.transform.rotation);
+            // Instantiate(explosionPlayer, other.transform.position, other.transform.rotation);
+
+            // GAME OVER
+            gameControllerScript.GameOver();
+        }
+        else
+        {
+            Instantiate(explosionSkull, this.transform.position, this.transform.rotation);
+            gameControllerScript.AddScore(score);
+        }
+
+        
 
         Destroy(other.gameObject); // Destroy other thing
         Destroy(this.gameObject); // Destroy this thing
